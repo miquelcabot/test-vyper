1875d2cfb5b3db14b386ab621c3edbd60b490296
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-07-13 20:39:04 +0200

    adapt test

--- tests/test_voting_escrow.py
+++ tests/test_voting_escrow.py
@@ -1,17 +1,9 @@
-from brownie import accounts, ERC20CRV, VotingEscrow
+from brownie import accounts, HelloWorld
 
 def test_deploy():
     account = accounts[0]
-    token_name = 'Ocean token'
-    token_symbol = 'OCEAN'
-    token_decimals = 18
-    version = '1.2'
+    counter = 20
 
-    erc20 = ERC20CRV.deploy(token_name, token_symbol, token_decimals, {'from': account})
+    hello_world = HelloWorld.deploy(counter, {'from': account})
 
-    voting_escrow = VotingEscrow.deploy(
-        erc20, token_name, token_symbol, version, {'from': account})
-
-    assert voting_escrow.name() == token_name
-    assert voting_escrow.symbol() == token_symbol
-    assert voting_escrow.version() == version
+    assert hello_world.counter() == counter
